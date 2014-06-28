3b61c51306ab75a2cdc3ccdc60f6a7cc1bcea83f
Hoon H <drawtree@me.com>
2014-06-28 15:46:34 +0900

    Step.

---
+++ Tester1/Tester1/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Tester1
+//
+//  Created by Hoon H. on 2014/06/28.
+//  Copyright (c) 2014 Eonil. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
