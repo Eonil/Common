c59863bdebb0d3a33737641ee722040cc4331d16
Hoon H <drawtree@me.com>
2014-06-28 13:36:33 +0900

    Step.

---
+++ AppKitSanitizer/AppKitSanitizer/BasicView.m
@@ -0,0 +1,51 @@
+//
+//  BasicView.m
+//  Spacetime
+//
+//  Created by Hoon H. on 14/5/26.
+//  Copyright (c) 2014 Eonil. All rights reserved.
+//
+
+#import "BasicView.h"
+
+@implementation BasicView
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self)
+	{
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+- (void)addConstraint:(NSLayoutConstraint *)constraint
+{
+	UNIVERSE_DEBUG_ASSERT_OBJECT_TYPE(constraint, NSLayoutConstraint);
+	
+	////
+	
+	[super addConstraint:constraint];
+}
+- (void)addConstraints:(NSArray *)constraints
+{
+	if (USE_DEBUGGING_ASSERTIONS)
+	{
+		UNIVERSE_DEBUG_ASSERT_OBJECT_TYPE(constraints, NSArray);
+		for (id a in constraints)
+		{
+			UNIVERSE_DEBUG_ASSERT_OBJECT_TYPE(a, NSLayoutConstraint);
+		}
+	}
+	
+	////
+	
+	[super addConstraints:constraints];
+}
+@end
